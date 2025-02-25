5d4c2e7e28f8436f8a075c15dca91702a844292a
DamriaNeelesh <damrianeelesh@gmail.com>
2025-02-25 14:37:53 +0530

    Minor formatting adjustment in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-600"
-        >
+        > 
           History and Milestones
         </a>
               <a
