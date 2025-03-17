6bfdd57ad7f1cf25789bf9acc89e8c17731975da
DamriaNeelesh <damrianeelesh@gmail.com>
2025-03-17 12:09:23 +0530

    testing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: false, 
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
